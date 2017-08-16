6c3bfae1e868664e46fc5a47ed14544e1fe1c226
ayavryk <ayavryk@gmail.com>
2017-08-16 10:38:52 +0300

    tmp app

--- src/app/containers/App/index.tsx
+++ src/app/containers/App/index.tsx
@@ -1,23 +1,45 @@
+import * as React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Helmet } from 'react-helmet';
+import { setMessage } from 'redux/modules/rCommand';
+import { setData } from 'redux/modules/rTable';
+import AppServerMessages from 'components/CApp/appServerMessages';
+import AppWrapper from 'components/CApp/appWrapper';
 declare var appConfig: any;
 
-import * as React from 'react';
-import { Helmet } from 'react-helmet';
-import { Header } from 'components';
-
-const style = require('./style.css');
-
-class App extends React.Component<any, any> {
-  public render() {
-    return (
-      <section className={style.AppContainer}>
-          <Helmet>
-            <title>{appConfig.head}</title>
-          </Helmet>
-        <Header />
-        {this.props.children}
-      </section>
-    );
-  }
+class App extends React.Component<any, void> {
+    public render() {
+        return (
+            <section>
+                <Helmet>
+                  <title>{appConfig.head}</title>
+                </Helmet>
+                <AppWrapper isChanged = {this.props.isChanged}>
+                    <AppServerMessages actions={this.props.actions} message={this.props.message} />
+                    {this.props.children}
+                </AppWrapper>
+            </section>
+        );
+    };
 }
 
-export { App }
+function mapStateToProps(state) {
+    return {
+        message: state.message,
+        isChanged: !!state.edit.isChanged,
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators({ setMessage, setData }, dispatch),
+    };
+}
+
+const exportApp = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(App);
+
+export {exportApp as App};
